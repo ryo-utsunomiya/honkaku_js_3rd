35bfcaa798be5578fd21addc9be0d2f53697f305
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-11 20:52:00 +0900

    Add string_es2015.js

---
+++ string_es2015.js
@@ -0,0 +1,9 @@
+const str = 'abcdefg';
+
+console.log(str.startsWith('a'));
+console.log(str.endsWith('a'));
+console.log(str.endsWith('bcd'));
+console.log(str.codePointAt(5));
+console.log(String.fromCodePoint(0x55));
+console.log('foo'.repeat(10));
+
